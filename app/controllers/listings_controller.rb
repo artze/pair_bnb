d1d41f5d2d847fdee75ada040c28dfe87f4d94b8
artze <arthurwtm@gmail.com>
2017-02-22 20:33:27 +0800

    Added listing submission functionality

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,27 @@ class ListingsController < ApplicationController
 	def index
 		@listings = Listing.paginate(page: params[:page], per_page: 10)
 	end
+
+	def new
+		@listing = Listing.new
+	end
+
+	def create
+		if (signed_in? == false)
+			redirect_to sign_in_path
+		else
+			@listing = current_user.listings.new(listing_params)
+			if @listing.save
+				'success'
+			else
+				'fail'
+			end
+		end
+	end
+
+	private
+
+	def listing_params
+		params[:listing].permit(:title, :description, :price, :address, :home_type, :room_type, :bedroom, :bathroom, :accommodate, :user_id)
+	end
 end
