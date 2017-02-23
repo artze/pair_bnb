b7e119013b72805d67822bc59eea2aff8dc0ab9e
artze <arthurwtm@gmail.com>
2017-02-23 17:10:35 +0800

    Added reservation creation and reservation date checking functionalities

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,4 +2,24 @@ class ReservationsController < ApplicationController
 	def new
 		@reservation = Reservation.new
 	end
+
+	def create
+		@reservation = current_user.reservations.new(reservation_params)
+		@reservation.listing_id = params[:listing_id]
+		if @reservation.check_reservation_dates
+			if @reservation.save
+				render text: 'reservation successful'
+			else
+				render text: 'reservation failed'
+			end
+		else
+			render text: 'reservation failed'
+		end
+	end
+
+	private
+
+	def reservation_params
+		params[:reservation].permit(:user_id, :listing_id, :booking_start, :booking_end)
+	end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :listing
+
+  def check_reservation_dates
+		self.listing.reservations.each do |reservation|
+			check_overlap = (reservation.booking_start - self.booking_end) * (self.booking_start - reservation.booking_end)
+			return false if check_overlap >= 0
+		end
+		return true
+	end
 end
