a22abef0ceaa157b3ba16afa9650310e910c96d2
artze <arthurwtm@gmail.com>
2017-02-23 14:53:13 +0800

    Added new reservation form interface

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create_from_omniauth'
 
-  resources :listings
+  resources :listings do
+    resource :reservation, only: [:new, :create, :edit, :update, :destroy]
+  end
 end
