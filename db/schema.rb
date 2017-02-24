e115a4180d2656506076f653d38c32a402bbb03c
artze <arthurwtm@gmail.com>
2017-02-24 15:49:13 +0800

    Added image array column to listings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223061046) do
+ActiveRecord::Schema.define(version: 20170224074703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 20170223061046) do
     t.integer  "bathroom"
     t.integer  "accommodate"
     t.integer  "user_id"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.text     "image",                                default: [],              array: true
     t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
