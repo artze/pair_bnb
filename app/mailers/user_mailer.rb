873ed6fcfeb914d3c431816e1863f396a233a9c3
artze <arthurwtm@gmail.com>
2017-02-28 18:26:39 +0800

    Implemented successful signup email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+	default from: 'pairbnb733@gmail.com'
+	def sign_up_email(user)
+		@user = user
+		email_with_name = %("#{@user.first_name + ' ' + @user.last_name}" <#{@user.email}>)
+		mail(to: email_with_name,
+			subject: 'Welcome to PairBnb!',
+			template_path: 'user_mailer',
+			template_name: 'sign_up_email')
+	end
 end
