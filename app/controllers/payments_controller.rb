a6e00e793c30deeafe0fc9af3ec9cbd5c41c3be5
artze <arthurwtm@gmail.com>
2017-03-01 18:02:43 +0800

    Implemented update reservation and save payment after successful transaction

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,7 +5,6 @@ class PaymentsController < ApplicationController
 
 	def checkout
     selected_reservation = Reservation.find_by(id: params[:reservation_id])
-    byebug
     amount_due = (selected_reservation.booking_end - selected_reservation.booking_start) * selected_reservation.listing.price
 		nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
 
@@ -18,6 +17,9 @@ class PaymentsController < ApplicationController
   	)
 
   	if result.success?
+      selected_reservation.confirmed!
+      @payment = Payment.new(reservation_id: selected_reservation.id, total_amount: amount_due)
+      @payment.save
   		redirect_to :root, flash: { success: 'Transaction successful!' }
   	else
   		redirect_to :root, flash: { error: 'Transaction failed. Please try again.' }
